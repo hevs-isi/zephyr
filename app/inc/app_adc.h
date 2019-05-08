87d4b285b8bfc9a78b559b9d2cd0187a126cf2c5
Marc Pignat <marc@pignat.org>
2019-05-08 10:35:03 +0200

    adc inputs working

--- app/inc/app_adc.h
+++ app/inc/app_adc.h
@@ -8,7 +8,13 @@ extern "C"
 {
 #endif
 
+void adc_init(void);
 void adc_test(void);
+
+uint16_t adc_measure_vbat(void);
+uint16_t adc_measure_charger(void);
+uint16_t adc_measure_sensor0(void);
+uint16_t adc_measure_sensor1(void);
 
 #ifdef __cplusplus
 }
--- app/src/app_adc.c
+++ app/src/app_adc.c
@@ -2,16 +2,31 @@
 #include <zephyr.h>
 #include <misc/printk.h>
 #include <pinmux/stm32/pinmux_stm32.h>
+#include <soc.h>
 #include <gpio.h>
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(adc, LOG_LEVEL_DBG);
 
 #include "app_adc.h"
-uint16_t temp_data[8];
+uint16_t adc_data[1];
 
 static const struct adc_channel_cfg channel_cfg[] =
 {
+	{
+		.gain				= ADC_GAIN_1,
+		.reference			= ADC_REF_INTERNAL,
+		.acquisition_time	= ADC_ACQ_TIME(ADC_ACQ_TIME_TICKS, 641),
+		.channel_id	   		= 0,
+		.differential		= 0,
+	},
+	{
+		.gain				= ADC_GAIN_1,
+		.reference			= ADC_REF_INTERNAL,
+		.acquisition_time	= ADC_ACQ_TIME(ADC_ACQ_TIME_TICKS, 641),
+		.channel_id	   		= 1,
+		.differential		= 0,
+	},
 	{
 		.gain				= ADC_GAIN_1,
 		.reference			= ADC_REF_INTERNAL,
@@ -26,67 +41,150 @@ static const struct adc_channel_cfg channel_cfg[] =
 		.channel_id	   		= 3,
 		.differential		= 0,
 	},
+	{
+		.gain				= ADC_GAIN_1,
+		.reference			= ADC_REF_INTERNAL,
+		.acquisition_time	= ADC_ACQ_TIME(ADC_ACQ_TIME_TICKS, 641),
+		.channel_id	   		= 4,
+		.differential		= 0,
+	},
 };
 
 const struct adc_sequence sequence[] =
 {
 	{
+		.channels	= BIT(0),
+		.buffer	  = adc_data,
+		.buffer_size = sizeof(adc_data),
+		.resolution  = 12,
+	},
+	{
+		.channels	= BIT(1),
+		.buffer	  = adc_data,
+		.buffer_size = sizeof(adc_data),
+		.resolution  = 12,
+	},
+	{
 		.channels	= BIT(2),
-		.buffer	  = temp_data,
-		.buffer_size = sizeof(temp_data),
+		.buffer	  = adc_data,
+		.buffer_size = sizeof(adc_data),
 		.resolution  = 12,
 	},
 	{
 		.channels	= BIT(3),
-		.buffer	  = temp_data,
-		.buffer_size = sizeof(temp_data),
+		.buffer	  = adc_data,
+		.buffer_size = sizeof(adc_data),
+		.resolution  = 12,
+	},
+	{
+		.channels	= BIT(4),
+		.buffer	  = adc_data,
+		.buffer_size = sizeof(adc_data),
 		.resolution  = 12,
 	},
 };
 
 static const struct pin_config pinconf[] =
 {
+	{STM32_PIN_PC0, STM32L4X_PINMUX_FUNC_PC1_ADC123_IN2},
 	{STM32_PIN_PC1, STM32L4X_PINMUX_FUNC_PC1_ADC123_IN2},
 	{STM32_PIN_PC2, STM32L4X_PINMUX_FUNC_PC2_ADC123_IN3},
+	{STM32_PIN_PC3, STM32L4X_PINMUX_FUNC_PC3_ADC123_IN4},
 };
 
-void adc_test(void)
+static uint16_t vdda_mv(uint16_t data)
 {
-	int ret;
+	return (uint32_t)3000*(*VREFINT_CAL_ADDR)/data;
+}
 
-	struct device *adc_dev = device_get_binding(DT_ADC_1_NAME);
-	struct device *dev = device_get_binding(DT_GPIO_STM32_GPIOA_LABEL);
-	ret = gpio_pin_configure(dev, 8, (GPIO_DIR_OUT));
-	gpio_pin_write(dev, 8, 1);
 
-	// FIXME : try to enable gpioc
-	//struct device *dev = device_get_binding(DT_GPIO_STM32_GPIOC_LABEL);
-	//ret = gpio_pin_configure(dev, 0, (GPIO_DIR_OUT));
-	// FIXME end
+/**
+ * \brief Convert an AD convertsion result to milivolts
+ *
+ * \param ref, the ADC conversion result of VREFINT
+ * \pram in, the ADC conversion result of the input
+ *
+ * \return the result in milivolts
+ *
+ * \see Chapter 18.4.34 in RM0351 for the computation.
+ */
+static uint16_t mes_mv(uint16_t ref, uint16_t in)
+{
+	uint64_t tmp;
 
+	/*
+	 * For maximum precision, all multipications are done before dividing.
+	 */
+	tmp = 3000;
+	tmp *= (*VREFINT_CAL_ADDR);
+	tmp *= in;
+
+	tmp /= ref;
+	tmp /= 4095;
+
+	return tmp;
+}
+
+static struct device *adc_dev;
+
+static uint16_t mes(const struct adc_sequence *s)
+{
+	uint16_t reference;
+	uint16_t in;
+	adc_read(adc_dev, &sequence[0]);
+	reference = adc_data[0];
+	adc_read(adc_dev, s);
+	in = adc_data[0];
+	LOG_DBG("ref:%"PRIu16" in: %"PRIu16, reference, in);
+	return mes_mv(reference, in);
+}
+
+uint16_t adc_measure_vbat(void)
+{
+	uint16_t tmp;
+	struct device *pin = device_get_binding(DT_GPIO_STM32_GPIOA_LABEL);
+	gpio_pin_configure(pin, 8, (GPIO_DIR_OUT));
+	gpio_pin_write(pin, 8, 0);
+	tmp = mes(&sequence[1]);
+	gpio_pin_configure(pin, 8, (GPIO_DIR_IN));
+	return tmp*133/33;
+}
+
+uint16_t adc_measure_charger(void)
+{
+	return mes(&sequence[4]);
+}
+
+uint16_t adc_measure_sensor0(void)
+{
+	return mes(&sequence[2]);
+}
+
+uint16_t adc_measure_sensor1(void)
+{
+	return mes(&sequence[3]);
+}
+
+void adc_init(void)
+{
+	adc_dev = device_get_binding(DT_ADC_1_NAME);
 	stm32_setup_pins(pinconf, ARRAY_SIZE(pinconf));
-
-
-	LOG_DBG("adc_dev = %p", adc_dev);
 
 	for (size_t i = 0; i < ARRAY_SIZE(channel_cfg); i++)
 	{
 		adc_channel_setup(adc_dev, &channel_cfg[i]);
 	}
 
-	while (1) {
-		for (size_t i = 0 ; i < ARRAY_SIZE(sequence); i++)
-		{
-			ret = adc_read(adc_dev, &sequence[i]);
-			k_sleep(100);
+	adc_read(adc_dev, &sequence[0]);
+	LOG_INF("VREFINT_DATA=%"PRIu16", VDDA=%"PRIu16, *VREFINT_CAL_ADDR, vdda_mv(adc_data[0]));
+}
 
-			if (ret != 0)
-			{
-				printk("adc_read() failed with code %d", ret);
-			} else {
-				printk("adc_read() value PC%d : %d\n", (int)i+1, temp_data[0]);
-			}
-		}
-		k_sleep(1000);
-	}
+void adc_test(void)
+{
+	adc_read(adc_dev, &sequence[0]);
+	LOG_INF("VDDA=%"PRIu16"\n", vdda_mv(adc_data[0]));
+	LOG_INF("adc_measure_vbat():%"PRIu16" mv", adc_measure_vbat());
+	LOG_INF("adc_measure_charger():%"PRIu16" mv", adc_measure_charger());
+	LOG_INF("adc_measure_sensor0():%"PRIu16" mv", adc_measure_sensor0());
+	LOG_INF("adc_measure_sensor1():%"PRIu16" mv", adc_measure_sensor1());
 }
